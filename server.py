bea439a1531dc3732a363cb690df64336f5746cb
Nikolay Rudenko <n_rudenko2@Nikolays-MacBook-Air.local>
2017-12-03 17:00:49 +0300

    simple flask server

--- server.py
+++ server.py
@@ -2,4 +2,12 @@
 from os import environ
 from flask import Flask
 app = Flask(__name__)
+@app.route("/")
+def hello():
+    return '<form action="/echo" method="GET"><input name="text"><input type="submit" value="Echo"></form>'
+ 
+@app.route("/echo")
+def echo(): 
+    return "You said: " + request.args.get('text', '')
+ 
 app.run(environ.get('PORT'))
